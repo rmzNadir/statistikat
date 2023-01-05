ef9c0c9bb47c11b2ba7050d31ef55881da2181aa
rmzNadir <dgzrz99@gmail.com>
2023-01-04 21:13:24 -0600

    fix: :bug: fix useLayoutEffect SSR warnings

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,10 +3,14 @@ import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useLayoutEffect } from 'react';
 
+const useSafeLayoutEffect =
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  typeof window === 'undefined' ? () => {} : useLayoutEffect;
+
 const Login: NextPage = () => {
   const { query } = useRouter();
 
-  useLayoutEffect(() => {
+  useSafeLayoutEffect(() => {
     signIn('spotify', {
       callbackUrl: (query?.redirect as string) ?? '/',
     });
