b6ce77280cd7fa14f61ddd454f41450a1e3e4cbd
rmzNadir <dgzrz99@gmail.com>
2023-01-08 20:56:25 -0600

    feat: :lipstick: update grid sizes

--- components/Common.ts
+++ components/Common.ts
@@ -2,6 +2,6 @@ import styled from '@emotion/styled';
 
 export const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(225px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
   gap: ${({ theme }) => theme.space[2]};
 `;
