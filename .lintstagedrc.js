ea38f30f000316fcb1be71475929b549e7d69fce
rmzNadir <dgzrz99@gmail.com>
2023-01-08 19:49:27 -0600

    ci: :wrench: update pre-commit hook

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+const buildCommand = (command, filenames) =>
+  `${command} ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
 module.exports = {
   // Type check TypeScript files
   '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
   // Lint & Prettify TS and JS files
   '**/*.(ts|tsx|js)': (filenames) => [
-    `yarn eslint --fix ${filenames.join(' ')}`,
-    `yarn prettier --write ${filenames.join(' ')}`,
+    buildCommand('next lint --fix --file', filenames),
+    buildCommand('yarn prettier --write', filenames),
   ],
 
   // Prettify only Markdown and JSON files
