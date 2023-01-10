9f74fce1a74a706bbfb21a89c4139b144f50436a
rmzNadir <dgzrz99@gmail.com>
2023-01-09 18:45:41 -0600

    fix: :bug: fix MobileRank sizing issues on route change

--- components/mobile-rank/styles.ts
+++ components/mobile-rank/styles.ts
@@ -1,18 +1,14 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  height: 100%;
+  min-height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-
-  & > div {
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    // 2 space from ChildrenContainer's left-padding, 2 space from item ItemGrid gap
-    max-height: ${({ theme }) => `calc(100% - ${theme.space[6]})`};
-    aspect-ratio: 1;
-  }
+  // Had to replace responsive styles due to mobile safari bug, since the
+  // card size is a constant in mobile due to word wrap this is fine.
+  // Card height is 4 rem - 0.5rem from ItemGrid's gap - 0.75rem from ChildrenContainer's padding
+  width: 2.75rem;
+  // Correct extra 0.5rem from 0.5rem - 0.75rem
+  padding-right: 0.25rem;
 `;
